445c3620c81591c0257e6a114f870b5f8823be75
samwanyua <samexample8@gmail.com>
2024-02-21 11:37:00 +0300

    Numbers

---
+++ numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+    // Numbers
+    printf("%f\n",234.23141 + 129.1234173);
+    
+    printf("%f\n", 5 + 1.1234173); //6.123417
+    
+    printf("%d\n", 5 + 1); //6
+    printf("%d\n", 5 / 4); //1
+    printf("%f\n", 5 / 4.0); //1.250000
+
+    // math functions
+    printf("%f\n", pow(2,2)); //4.000000
+    printf("%f\n", sqrt(64)); //8.000000
+    printf("%f\n", ceil(2.234)); //3.000000
+    printf("%f\n", floor(2.234)); //2.000000
+
+
+
+
+
+    return 0;
+}
