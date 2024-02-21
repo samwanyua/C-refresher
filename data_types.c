e326e2903b36137cba2c0a4a3f2e5eff496989d4
samwanyua <samexample8@gmail.com>
2024-02-21 11:10:55 +0300

    Data types

---
+++ data_types.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // Data types
+    
+    // numbers -> integer and floats
+    int age = 40;
+
+    // decimal numbers -> double and float
+    double gpa = 3.7;
+
+    // characters
+    char grade = 'C'; //single character
+
+    char userName [] = "samwanyua" //multiple characters
+
+    
+
+
+
+
+
+
+    return 0;
+}
