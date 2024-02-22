0b798f3ca49b061cb6b3cce795079af6c5031838
samwanyua <samexample8@gmail.com>
2024-02-22 11:06:52 +0300

    Comments

---
+++ comments.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    
+    //This is a comment. It will be ignored by the compiler
+    printf("Hello world!");
+     
+     /* This 
+        is 
+        a 
+        multiline
+        commment
+        */
+
+    return 0;
+}
